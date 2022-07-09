5eebc5fbaf012a0381334486092b14d62c856ec5
Nullll222 <108679673+Nullll222@users.noreply.github.com>
2022-07-09 15:46:26 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -44,7 +44,7 @@ module.exports = {
   protectedRoutes: ['/🌞 Private folder/u-need-a-password', '/🥟 Some test files/Protected route'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
-  email: '无',
+  email: 'null',
 
   // [OPTIONAL] This is an array of names and links for setting your social information and links.
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
@@ -63,7 +63,7 @@ module.exports = {
   
   links: [
     {
-      name: '暂无联系方式',
+      name: 'Null',
       link: '',
     }
   ],
